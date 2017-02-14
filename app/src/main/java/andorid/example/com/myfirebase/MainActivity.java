0af27e06132feb6c33d254045c09c05cd9749b9a
Sabin-HP <mhr_sabin@hotmail.com>
2017-02-14 11:36:11 +0545

    First Commit

--- app/src/main/java/andorid/example/com/myfirebase/MainActivity.java
+++ app/src/main/java/andorid/example/com/myfirebase/MainActivity.java
@@ -1,7 +1,9 @@
-ug.stpackage andorid.example.com.myfirebase;
+package andorid.example.com.myfirebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import andorid.example.com.myfirebase.R;
 
 public class MainActivity extends AppCompatActivity {
 
